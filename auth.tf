aa642b43934b7430cc541f9ce5892cd5747309b7
Iuri Severo <nobreiuri@gmail.com>
2021-01-25 18:21:47 -0300

    Remove load_config_file from auth.tf provider

--- auth.tf
+++ auth.tf
@@ -27,7 +27,6 @@ data "google_client_config" "default" {
   Configure provider
  *****************************************/
 provider "kubernetes" {
-  load_config_file       = false
   host                   = "https://${local.cluster_endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
